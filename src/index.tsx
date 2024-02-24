ee0ec0f55dcc757de027e7dd17070d3a2688da98
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-24 11:55:36 +0700

    common define for page request

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import Modal from 'react-modal';
+import './index.css';
 import CONFIG_KEYS from './config';
-import React from 'react';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import { RouterProvider } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import AppRouter from './routes';
+// import { Provider } from 'react-redux';
+// import { store, persister } from './redux/store';
+import { ThemeProvider } from '@material-tailwind/react';
+import { ToastContainer } from 'react-toastify';
+// import { PersistGate } from 'redux-persist/intergation/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -12,7 +19,10 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <GoogleOAuthProvider clientId={CONFIG_KEYS.GOOGLE_AUTH_CLIENT_ID}>
-            <RouterProvider router={AppRouter}></RouterProvider>
+            <ThemeProvider>
+                <RouterProvider router={AppRouter}></RouterProvider>
+                <ToastContainer />
+            </ThemeProvider>
         </GoogleOAuthProvider>
     </React.StrictMode>,
 );
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,14 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import { Student } from './App';
+import ErrorElement from './components/common/error-element';
+
+const LazyListCourse = lazy(() => import('./components/pages/coure-pages/list-course'));
 
 const AppRouter = createBrowserRouter([
     {
         path: '/',
         element: <Student />,
+        errorElement: <ErrorElement />,
     },
 ]);
