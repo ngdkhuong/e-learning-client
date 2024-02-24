c610fb2d70d11a9a3d60f27086344886c2534ba5
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-24 17:16:27 +0700

    updated

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,34 @@ import { createBrowserRouter } from 'react-router-dom';
 import { Student } from './App';
 import ErrorElement from './components/common/error-element';
 
-const LazyListCourse = lazy(() => import('./components/pages/coure-pages/list-course'));
+const LazyListCourse = lazy(() => import('./components/pages/course-pages/list-course'));
+
+const LazyStudentHomePage = lazy(() => import('./components/pages/students/student-home-page.tsx'));
 
 const AppRouter = createBrowserRouter([
     {
         path: '/',
         element: <Student />,
         errorElement: <ErrorElement />,
+        children: [
+            {
+                path: '/',
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <LazyStudentHomePage />
+                    </Suspense>
+                ),
+            },
+            {
+                path: '/',
+                element: (
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <LazyListCourse />
+                    </Suspense>
+                ),
+            },
+        ],
     },
 ]);
+
+export default AppRouter;
