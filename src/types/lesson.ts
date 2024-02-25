91eb89b6d56dd1200f00b1da2308fdb866208862
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-25 18:34:34 +0700

    data types api create

---
+++ src/types/lesson.ts
@@ -0,0 +1,20 @@
+export interface FormValuesLesson {
+    [key: string]: any;
+    title: string;
+    description: string;
+    about: string;
+    studyMaterials: string;
+    contents: string;
+    duration: string;
+    questions: Question[];
+}
+
+interface Question {
+    question: string;
+    options: Option[];
+}
+
+interface Option {
+    option: string;
+    isCorrect: boolean;
+}
---
+++ src/types/student.ts
@@ -0,0 +1,9 @@
+export interface StudentData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    mobile: string;
+    interests: string[];
+}
