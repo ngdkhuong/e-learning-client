c38b3c643765f747c936f3267bc1a21045e4bb27
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-25 15:47:20 +0700

    decode jwt logged in

---
+++ src/utils/decode.ts
@@ -0,0 +1,25 @@
+import { JwtPayload, jwtDecode } from 'jwt-decode';
+
+interface Payload {
+    email: string;
+    Id: string;
+    role: string;
+}
+
+interface DecodedToken extends JwtPayload {
+    exp: number;
+    iat: number;
+    payload: Payload;
+}
+
+const decodeJwtToken = (jwtToken: string): DecodedToken | null => {
+    try {
+        const decodedToken = jwtDecode<DecodedToken>(jwtToken);
+        return decodedToken;
+    } catch (error) {
+        console.error('Error decoding JWT token:', error);
+        return null;
+    }
+};
+
+export default decodeJwtToken;
