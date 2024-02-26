300ae55770acb410126bc2113c85c7a87ace8b77
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-26 11:26:33 +0700

    environment constant

---
+++ src/constants/common.ts
@@ -0,0 +1,3 @@
+export const client_id = '691942546538-7kn09nrsjusp9o5bio16sd54td75vpv2.apps.googleusercontent.com';
+export const redirect_uri = 'http://localhost:3000';
+export const USER_AVATAR = 'https://res.cloudinary.com/dwucedjmy/image/upload/v1690203086/images_2_d4e6fp.jpg';
