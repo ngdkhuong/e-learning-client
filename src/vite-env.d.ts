13726ab1c0b18628417583db508dfab2d26ce32a
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-26 11:17:39 +0700

    config env

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    REACT_APP_CLIENT_ID: string;
+    REACT_APP_STRIPE_PUBLISHABLE_KEY: string;
+    REACT_APP_REDIRECT_URI: string;
+    REACT_APP_API_BASE_URL: string;
+    // Add more environment variables as needed
+}
