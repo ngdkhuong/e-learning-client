64c9519adb5766997126f11167b4fe9aeb0c0a00
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-26 08:09:55 +0700

    add interface auth

---
+++ src/api/types/instructor/auth-interface.ts
@@ -0,0 +1,19 @@
+export interface StudentRegisterData {
+    [key: string]: any;
+    firstName: string;
+    lastName: string;
+    email: string;
+    mobile: string;
+    qualification: string;
+    subjects: string;
+    experience: string;
+    skills: string;
+    about: string;
+    password: string;
+    images?: File[];
+}
+
+export interface InstructorLoginData {
+    email: string;
+    password: string;
+}
