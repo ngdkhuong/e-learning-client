b4c26aa36a376613e3f435a0604dd05659a1e9d9
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-27 17:02:41 +0700

    update axios custom error

--- src/api/middlewares/interceptor.ts
+++ src/api/middlewares/interceptor.ts
@@ -17,7 +17,21 @@ axiosInstance.interceptors.response.use(
                 throw new CustomApiError('Bad Request', data);
             } else if (status === 401) {
                 throw new CustomApiError('Unauthorized', data);
+            } else if (status === 404) {
+                throw new CustomApiError('Not found', data);
+            } else if (status === 409) {
+                throw new CustomApiError('Conflict', data);
+            } else {
+                throw new CustomApiError(`Request failed with status ${status}`, data);
             }
+        } else if (error.request) {
+            throw new CustomApiError(`No response received`, error.request);
+        } else {
+            console.log('Error:', error.message);
         }
+
+        return Promise.reject(error);
     },
 );
+
+export default axiosInstance;
