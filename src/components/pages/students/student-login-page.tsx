669a484fdcdc6969f0bd90d8fbc4042216c2372d
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-02-29 16:28:40 +0700

    completed

--- src/components/pages/students/student-login-page.tsx
+++ src/components/pages/students/student-login-page.tsx
@@ -69,10 +69,55 @@ const StudentLoginPage: React.FC = () => {
                                     <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
                                 </div>
                             </div>
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label
+                                        htmlFor="password"
+                                        className="block text-sm font-medium leading-6 text-gray-900"
+                                    >
+                                        Password
+                                    </label>
+                                    <div className="text-sm">
+                                        <a href="/" className="font-semibold text-blue-600 hover:text-indigo-500">
+                                            Forgot password?
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="mt-2">
+                                    <Field
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-700 focus-visible:outline-none focus-visible:ring-blue-600 sm:text-sm sm:leading-6"
+                                    />
+                                    <ErrorMessage name="password" component="div" className="text-red-500 text-sm" />
+                                </div>
+                            </div>
+                            <div>
+                                <div className="m-5">
+                                    <GoogleAuthComponent />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                >
+                                    Sign in
+                                </button>
+                            </div>
                         </Form>
                     </Formik>
+                    <p className="mt-10 text-center text-sm text-gray-500">
+                        Do not have an account?
+                        <Link to="/register" className="font-semibold leading-6 text-blue-600 hover:text-indigo-500">
+                            &nbsp; Sign up
+                        </Link>
+                    </p>
                 </div>
             </div>
         </div>
     );
 };
+
+export default StudentLoginPage;
---
+++ src/validations/student.ts
@@ -0,0 +1,11 @@
+import * as Yup from 'yup';
+
+export const PasswordValidationSchema = Yup.object().shape({
+    currentPassword: Yup.string().required('Current password is required'),
+    newPassword: Yup.string().required('New password is required').min(8, 'Password must be at least 8 characters'),
+    repeatPassword: Yup.string()
+        .required('Confirm password is required')
+        .test('password-match', 'Password must match', function (value) {
+            return value === this.parent.newPassword;
+        }),
+});
