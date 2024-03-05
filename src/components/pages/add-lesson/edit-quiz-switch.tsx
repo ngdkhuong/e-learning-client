174500c77239b75c06f3419970154df352c4a03b
ngdkhuong <nguyenkhuong31072002@gmail.com>
2024-03-05 11:38:03 +0700

    lesson api

---
+++ src/components/pages/add-lesson/edit-quiz-switch.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Typography } from '@material-tailwind/react';
+import { Switch } from '@material-tailwind/react';
+
+type QuizzesSwitchProps = {
+    editQuiz: boolean;
+    setEditQuiz: (editQuiz: boolean) => void;
+};
+
+const EditQuizSwitch: React.FC<QuizzesSwitchProps> = ({ editQuiz, setEditQuiz }) => {
+    const handleSwitchChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        const isChecked = event.target.checked;
+        setEditQuiz(isChecked);
+    };
+
+    return (
+        <Switch
+            checked={editQuiz}
+            onChange={handleSwitchChange}
+            label={
+                <div>
+                    <Typography color="blue-gray" className="font-medium" placeholder={undefined}>
+                        Edit Quiz?
+                    </Typography>
+                    <Typography variant="small" color="gray" className="font-normal" placeholder={undefined}>
+                        You'll be able to edit quizzes for the lesson.
+                    </Typography>
+                </div>
+            }
+            containerProps={{
+                className: '-mt-5',
+            }}
+            crossOrigin={undefined}
+        />
+    );
+};
+
+export default EditQuizSwitch;
---
+++ src/components/pages/add-lesson/quiz-switch.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Typography } from '@material-tailwind/react';
+import { Switch } from '@material-tailwind/react';
+
+type QuizzesSwitchProps = {
+    addQuiz: boolean;
+    setAddQuiz: (addQuiz: boolean) => void;
+};
+
+const QuizSwitch: React.FC<QuizzesSwitchProps> = ({ addQuiz, setAddQuiz }) => {
+    const handleSwitchChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        const isChecked = event.target.checked;
+        setAddQuiz(isChecked);
+    };
+
+    return (
+        <Switch
+            checked={addQuiz}
+            onChange={handleSwitchChange}
+            label={
+                <div>
+                    <Typography color="blue-gray" className="font-medium" placeholder={undefined}>
+                        Add Quiz?
+                    </Typography>
+                    <Typography variant="small" color="gray" className="font-normal" placeholder={undefined}>
+                        You'll be able to add quizzes for the lesson.
+                    </Typography>
+                </div>
+            }
+            containerProps={{
+                className: '-mt-5',
+            }}
+            crossOrigin={undefined}
+        ></Switch>
+    );
+};
+
+export default QuizSwitch;
